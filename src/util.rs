26247394adc3d82b47768188a77f1238b3cc5990
Ilya Averyanov <i.averyanov@geoscan.aero>
2019-11-12 13:42:40 +0300

    pub(crate) to util

--- src/util.rs
+++ src/util.rs
@@ -5,7 +5,7 @@ use ::libc::{c_int,c_char};
 
 use std::os::unix::prelude::*;
 
-pub fn ptr_to_os_str<'a>(ptr: *const c_char) -> Option<&'a OsStr> {
+pub(crate) fn ptr_to_os_str<'a>(ptr: *const c_char) -> Option<&'a OsStr> {
     if !ptr.is_null() {
         Some(unsafe { ptr_to_os_str_unchecked(ptr) })
     }
@@ -14,18 +14,18 @@ pub fn ptr_to_os_str<'a>(ptr: *const c_char) -> Option<&'a OsStr> {
     }
 }
 
-pub unsafe fn ptr_to_os_str_unchecked<'a>(ptr: *const c_char) -> &'a OsStr {
+pub(crate) unsafe fn ptr_to_os_str_unchecked<'a>(ptr: *const c_char) -> &'a OsStr {
     OsStr::from_bytes(slice::from_raw_parts(ptr as *const u8, ::libc::strlen(ptr) as usize))
 }
 
-pub fn os_str_to_cstring<T: AsRef<OsStr>>(s: T) -> crate::Result<CString> {
+pub(crate) fn os_str_to_cstring<T: AsRef<OsStr>>(s: T) -> crate::Result<CString> {
     match CString::new(s.as_ref().as_bytes()) {
         Ok(s) => Ok(s),
-        Err(_) => return Err(crate::error::from_errno(::libc::EINVAL))
+        Err(_) => Err(crate::error::from_errno(::libc::EINVAL))
     }
 }
 
-pub fn errno_to_result(errno: c_int) -> crate::Result<()> {
+pub(crate) fn errno_to_result(errno: c_int) -> crate::Result<()> {
     match errno {
         0 => Ok(()),
         e => Err(crate::error::from_errno(e))
